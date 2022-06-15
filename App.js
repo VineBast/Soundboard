f2eaeb0e70067bdd09c24bbfcb3a5bdc53efc59c
VineBast <vine.bast@gmail.com>
2022-06-15 11:59:39 +0200

    finalisation bottom tab UI

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import persistStore from 'redux-persist/es/persistStore';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import Record from './src/components/Record';
 import Search from './src/components/Search';
 import Sampler from './src/components/Sampler';
@@ -20,23 +21,47 @@ const App = () => {
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen 
-            name='Search'
-            component={Search}
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarShowLabel: false,
+              headerShown: false,
+              tabBarStyle: { backgroundColor: '#B5BDA5' },
+              tabBarIcon: ({ focused }) => {
+                let color = focused ? '#011303' : '#8B9D6C'
+                const icons = {
+                  Search: 'search-outline',
+                  Sampler: 'play-circle-outline',
+                  Record: 'recording-outline'
+                };
+                return (
+                  <Ionicons
+                    name={icons[route.name]}
+                    color={color}
+                    size={35}
+                  />
+                );
+              },
+            })}
+          >
+            <Tab.Screen
+              name='Search'
+              component={Search}
+
             />
-            <Tab.Screen 
-            name='Sampler'
-            component={Sampler}
+            <Tab.Screen
+              name='Sampler'
+              component={Sampler}
+
             />
-            <Tab.Screen 
-            name='Record'
-            component={Record}
+            <Tab.Screen
+              name='Record'
+              component={Record}
+
             />
           </Tab.Navigator>
         </NavigationContainer>
       </PersistGate>
-    </Provider>
+    </Provider >
   )
 }
 
